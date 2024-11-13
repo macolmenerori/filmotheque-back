dbefc43ba7bd3e54266cf88faac14497b18c5107
macolmenerori <miguelangelcolmenero@gmail.com>
2024-11-13 12:16:25 +0100

    Fixed methodNotAllowed

--- src/routes/moviesRouter.ts
+++ src/routes/moviesRouter.ts
@@ -17,26 +17,32 @@ import { methodNotAllowed } from '../utils/methodNotAllowed';
 const upload = multer({ dest: 'uploads/' });
 const router = express.Router();
 
-// router.route('/').get((req, res) => {
-//   res.status(200).json({
-//     status: 'success',
-//     message: 'Movies route'
-//   });
-// });
+router
+  .route('/searchmovie')
+  .get(protect, searchTraktMovieByTitle)
+  .all(methodNotAllowed(['GET']));
 
-router.route('/searchmovie').get(protect, searchTraktMovieByTitle);
-//.all(methodNotAllowed(['GET']));
 router
   .route('/movie')
   .get(protect, getAllMovies)
   .post(protect, addMovieToCollection)
   .patch(protect, updateMovie)
-  .delete(protect, deleteMovieFromCollection); // TODO: validation on POST body
+  .delete(protect, deleteMovieFromCollection)
+  .all(methodNotAllowed(['GET', 'POST', 'PATCH', 'DELETE'])); // TODO: validation on POST body
 
-router.route('/fullmovie').get(protect, getFullMovie);
+router
+  .route('/fullmovie')
+  .get(protect, getFullMovie)
+  .all(methodNotAllowed(['GET']));
 
-router.route('/exportuserdata').get(protect, exportUserData);
+router
+  .route('/exportuserdata')
+  .get(protect, exportUserData)
+  .all(methodNotAllowed(['GET']));
 
-router.route('/importuserdata').post(protect, upload.single('file'), importUserData);
+router
+  .route('/importuserdata')
+  .post(protect, upload.single('file'), importUserData)
+  .all(methodNotAllowed(['POST']));
 
 export default router;
--- src/utils/methodNotAllowed.ts
+++ src/utils/methodNotAllowed.ts
@@ -3,10 +3,11 @@ import { NextFunction, Request, Response } from 'express';
 import { HTTPMethod } from './consts';
 
 export const methodNotAllowed =
-  (allowedMethods: HTTPMethod[]) => (req: Request, res: Response, next: NextFunction) => {
+  (allowedMethods: HTTPMethod[]) =>
+  (req: Request, res: Response, next: NextFunction): void => {
     if (!allowedMethods.includes(req.method as HTTPMethod)) {
       res.setHeader('Allow', allowedMethods.join(', '));
-      return res.status(405).json({ status: 'fail', message: 'Method not allowed' });
+      res.status(405).json({ status: 'fail', message: 'Method not allowed' });
     }
     next();
   };
